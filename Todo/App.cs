c6999928fce1dd48fd75f5d50c92d6a1e76cebb9
Romancini <romancini@hpe.com>
2017-12-15 19:43:35 -0200

    first commit

---
+++ Todo/App.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Diagnostics;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Todo
+{
+	public class App : Application
+	{
+		static TodoItemDatabase database;
+
+		public App()
+		{
+			Resources = new ResourceDictionary();
+			Resources.Add("primaryGreen", Color.FromHex("1E90FF"));
+			Resources.Add("primaryDarkGreen", Color.FromHex("6FA22E"));
+
+			var nav = new NavigationPage(new TodoListPage());
+			nav.BarBackgroundColor = (Color)App.Current.Resources["primaryGreen"];
+			nav.BarTextColor = Color.White;
+
+			MainPage = nav;
+		}
+
+		public static TodoItemDatabase Database
+		{
+			get
+			{
+				if (database == null)
+				{
+					database = new TodoItemDatabase(DependencyService.Get<IFileHelper>().GetLocalFilePath("TodoSQLite.db3"));
+				}
+				return database;
+			}
+		}
+
+		public int ResumeAtTodoId { get; set; }
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
+
